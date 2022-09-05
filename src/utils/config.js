70b4d41c40f4d3377d428ce9d93348c5a4a2c148
Thierry <southasa.thierry@gmail.com>
2022-09-05 17:36:37 +0200

    ✨  F02 : API > Create utils

---
+++ src/utils/config.js
@@ -0,0 +1,8 @@
+export const API_URL = process.env.REACT_APP_API_URL
+export const apiKey = 'b609fa470a8164fa113cb1e3e1f846e2'
+export const lang = 'fr-fr'
+export const imagePath = process.env.REACT_APP_IMAGE_URL
+export const imagePathOriginal = `${imagePath}/original`
+export const imagePath400 = `${imagePath}/w400`
+export const TYPE_TV = 'tv'
+export const TYPE_MOVIE = 'movie'
